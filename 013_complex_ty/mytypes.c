ce7b20cf9fe9fb64375b9316f37113ad9161ab7c
gl137 <gl137@duke.edu>
2021-08-26 10:30:17 -0400

    2nd

--- 013_complex_ty/mytypes.c
+++ 013_complex_ty/mytypes.c
@@ -1,17 +1,19 @@
-struct _complex_num_t {
-	double real_part;
-	double imaginary_part;
+struct _complex_num_t
+{
+        double real_part;
+        double imaginary_part;
 };
 
 typedef struct _complex_num_t complex_num_t;
 
-enum _animal_t {
-	CAT,
-	FISH,
-	SNAKE,
-	DOG,
-	HORSE
+enum _animal_t
+{
+        CAT,
+        FISH,
+        SNAKE,
+        DOG,
+        HORSE
 };
 
-typedef enum _animal_t enum_animal_t;
+typedef enum _animal_t animal_t;
 
