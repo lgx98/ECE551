f2062749ed1eb3f3f43a5bbb93dfc49f0493313f
gl137 <gl137@duke.edu>
2021-09-16 12:24:07 -0400

    1st

--- 035_array_max/arrayMax.c
+++ 035_array_max/arrayMax.c
@@ -2,7 +2,17 @@
 #include <stdlib.h>
 
 int * arrayMax(int * array, int n) {
-  return NULL;
+  if(n==0)
+    return NULL;
+  int maxNum = *array;
+  int maxIndex = 0;
+  for(int i = 0; i < n; i++) {
+    if(*(array+i)>maxNum) {
+      maxNum=*(array+i);
+      maxIndex=i;
+    }
+  }
+  return array+maxIndex;
 }
 
 void doTest(int * array, int n) {
