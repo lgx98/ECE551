a00538152f563374b45725ca429db84cdc7f6394
gl137 <gl137@duke.edu>
2021-09-20 22:27:50 -0400

    1st

--- 041_reverse_str/reverse.c
+++ 041_reverse_str/reverse.c
@@ -3,7 +3,16 @@
 #include <string.h>
 
 void reverse(char * str) {
-  //WRITE ME!
+  if (str == NULL)
+    return;
+  size_t len = strlen(str);
+  for (size_t i = 0; i < len / 2; ++i) {
+    char temp;
+    temp = str[i];
+    str[i] = str[len - i - 1];
+    str[len - i - 1] = temp;
+  }
+  return;
 }
 
 int main(void) {
@@ -13,7 +22,8 @@ int main(void) {
   char str3[] = "Captain's log, Stardate 42523.7";
   char str4[] = "Hello, my name is Inigo Montoya.";
   char str5[] = "You can be my wingman anyday!";
-  char str6[] = "Executor Selendis! Unleash the full power of your forces! There may be no tomorrow!";
+  char str6[] = "Executor Selendis! Unleash the full power of your forces! There may be "
+                "no tomorrow!";
   char * array[] = {str0, str1, str2, str3, str4, str5, str6};
   for (int i = 0; i < 7; i++) {
     reverse(array[i]);
