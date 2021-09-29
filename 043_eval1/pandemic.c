c39722ba4a17509c67eb2e3953e6551b2e453807
gl137 <gl137@duke.edu>
2021-09-29 13:23:04 -0400

    S1 tests

--- 043_eval1/pandemic.c
+++ 043_eval1/pandemic.c
@@ -1,6 +1,8 @@
 #include "pandemic.h"
 
-#include "stdio.h"
+#include <errno.h>
+#include <stdbool.h>
+#include <stdio.h>
 
 // print the error message to stderr, then exit with failure.
 void error(const char * msg) {
@@ -19,7 +21,7 @@ country_t parseLine(char * line) {
   if (line == NULL)
     error("NULL Input: line");
 
-  // If line is empty, this should be invalid?
+  // If line is empty, this should be invalid
   if ((*line) == '\0')
     error("Empty Input");
 
@@ -35,14 +37,22 @@ country_t parseLine(char * line) {
     ans.name[i] = *(line + i);
     ++i;
   }
-  // Empty country name is invalid
-  if (i == 1)
-    error("Empty Country Name");
+
+  // Empty country name is VALID
+  //if (i == 1)
+  //  error("Empty Country Name");
+
   ans.name[i] = '\0';
   // At this time, *(line + i) should be pointing at char ',', so move it number section.
   line += (i + 1);
 
   // Parse the number.
+  uint64_t num = strtoull(line, NULL, 10);
+  if (errno == ERANGE)
+    error("Number Out of Range");
+
+  ans.population = num;
+
   return ans;
 }
 
@@ -53,12 +63,13 @@ void calcRunningAvg(unsigned * data, size_t n_days, double * avg) {
   // data should not be a NULL pointer.
   if (data == NULL)
     error("NULL Input: data");
-  // after thorough testing against example implementation,
-  // the right behavior should be a single average for n_days
 
+  // The right behavior should be quitting without doing anything.
   // n_days should be at least 7.
-  //if (n_days < period)
+  if (n_days < period)
+    return;
   //  error("Data Length Less Than Averging Period");
+
   // avg should not be a NULL pointer.
   if (data == NULL)
     error("NULL Input: avg");
@@ -72,18 +83,65 @@ void calcRunningAvg(unsigned * data, size_t n_days, double * avg) {
     }
     *(avg + i) = (double)sum / (double)period;
   }
+
+  return;
 }
 
 void calcCumulative(unsigned * data, size_t n_days, uint64_t pop, double * cum) {
-  uint64_t sum = 0;
-
   // Sanity checks:
   // data should not be a NULL pointer.
+  if (data == NULL)
+    error("NULL Input: data");
+
+  // cum should not be a NULL pointer.
+  if (cum == NULL)
+    error("NULL Input: cum");
+
+  uint64_t sum = 0;
+
+  // If n_days==0, it will not enter the loop, and return without doing anything.
+  for (size_t i = 0; i < n_days; ++i) {
+    sum += *(data + i);
+    *(cum + i) = (double)sum * 1e5 / pop;
+  }
+
+  return;
 }
 
 void printCountryWithMax(country_t * countries,
                          size_t n_countries,
                          unsigned ** data,
                          size_t n_days) {
-  //WRITE ME
+  // Sanity checks:
+  // countries should not be a NULL pointer.
+  if (countries == NULL)
+    error("NULL pointer: countries");
+
+  // data should not be a NULL pointer.
+  if (data == NULL)
+    error("NULL pointer: data");
+
+  // n_countries should not be 0, if that happens, return whitout doing anything
+  if (n_countries == 0)
+    return;
+
+  for (size_t j = 0; j < n_days, ++j) {
+    size_t max_i = 0;
+    bool tie = 0;
+    for (size_t i = 1; i < n_countries; ++i) {
+      // If there is another country that has the same cases, set tie to 1.
+      tie |= (data[i][j] == data[max_i][j]);
+      if (data[i][j] > data[max_i][j]) {
+        max_i = i;
+        tie = 0;
+      }
+    }
+    if (tie)
+      printf("There is a tie between at least two countries\n");
+    else
+      printf(
+          "%s has the most daily cases with %u\n", countries[max_i].name, data[max_i][j]);
+  }
+
+  return;
 }
