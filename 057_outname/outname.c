b543d2f9f0cff8a23aa963df148d509179713d3d
gl137 <gl137@duke.edu>
2021-10-06 21:44:14 -0400

    2nd

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -10,6 +10,6 @@ char * computeOutputFileName(const char * inputName) {
   size_t len = strlen(inputName) + strlen(suffix) + 1;
   char * outputName = malloc(len * sizeof(*outputName));
   memcpy(outputName, inputName, strlen(inputName));
-  memcpy(outputName + strlen(inputName), suffix, strlen(suffix));
+  memcpy(outputName + strlen(inputName), suffix, strlen(suffix) + 1);
   return outputName;
 }
