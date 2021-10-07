35dcd9735e150df63f2751b4d90b7c9770125d3e
gl137 <gl137@duke.edu>
2021-10-06 21:41:13 -0400

    1st

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -1,8 +1,15 @@
+#include "outname.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  const char * suffix = ".counts";
+  size_t len = strlen(inputName) + strlen(suffix) + 1;
+  char * outputName = malloc(len * sizeof(*outputName));
+  memcpy(outputName, inputName, strlen(inputName));
+  memcpy(outputName + strlen(inputName), suffix, strlen(suffix));
+  return outputName;
 }
