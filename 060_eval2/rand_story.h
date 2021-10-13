23c22d8a1d9d2eb8fb9eea8d9d56805b876e9de0
gl137 <gl137@duke.edu>
2021-10-13 15:29:24 -0400

    created main functions for steps.

--- 060_eval2/rand_story.h
+++ 060_eval2/rand_story.h
@@ -1,8 +1,37 @@
 #ifndef __RAND_STORY_H__
 #define __RAND_STORY_H__
 
+#include <stdio.h>
+
 #include "provided.h"
 
 //any functions you want your main to use
+
+typedef enum _tokenType_t { STRING, BLANK } tokenType_t;
+
+typedef struct _token_t {
+  tokenType_t type;
+  char * str;
+} token_t;
+
+token_t * token_new(const tokenType_t type, const char * str);
+
+void token_delete(token_t token);
+
+typedef struct _tokenArr_t {
+  token_t * tokens;
+  size_t n_tokens;
+  size_t n_avail;
+} tokenArr_t;
+
+tokenArr_t * tokenArr_new(void);
+
+void tokenArr_delete(tokenArr_t * arr);
+
+void tokenArr_push_back(tokenArr_t * arr, tokenArr_t token);
+
+tokenArr_t * parseStory(FILE * f);
+
+catarray_t * parseCat(FILE * f);
 
 #endif
---
+++ 060_eval2/story-step3.c
@@ -0,0 +1,5 @@
+#include "rand_story.h"
+
+int main(int argc, char ** argv) {
+  return EXIT_SUCCESS;
+}
