4634afbda3aa81beca18753f9bd8ee7b38525374
gl137 <gl137@duke.edu>
2021-10-25 22:33:46 -0400

    1st

---
+++ 084_expr_eval2/expr.hpp
@@ -0,0 +1,68 @@
+#ifndef __EXPR_HPP__
+#define __EXPR_HPP__
+
+#include <sstream>
+#include <string>
+
+class Expression {
+ public:
+  virtual std::string toString() const = 0;
+  virtual ~Expression() {}
+};
+
+class NumExpression : public Expression {
+ private:
+  long num;
+
+ public:
+  NumExpression(long n) : num(n) {}
+  virtual std::string toString() const {
+    std::stringstream ss;
+    ss << num;
+    std::string s = ss.str();
+    return s;
+  }
+};
+
+class OpExpression : public Expression {
+  char op;
+  Expression * lhs;
+  Expression * rhs;
+
+ public:
+  OpExpression(char op, Expression * lhs, Expression * rhs) :
+      op(op),
+      lhs(lhs),
+      rhs(rhs) {}
+  virtual std::string toString() const {
+    std::stringstream ss;
+    ss << '(' << lhs->toString() << ' ' << op << ' ' << rhs->toString() << ')';
+    return ss.str();
+  }
+  virtual ~OpExpression() {
+    delete lhs;
+    delete rhs;
+  }
+};
+
+class PlusExpression : public OpExpression {
+ public:
+  PlusExpression(Expression * lhs, Expression * rhs) : OpExpression('+', lhs, rhs) {}
+};
+
+class MinusExpression : public OpExpression {
+ public:
+  MinusExpression(Expression * lhs, Expression * rhs) : OpExpression('-', lhs, rhs) {}
+};
+
+class TimesExpression : public OpExpression {
+ public:
+  TimesExpression(Expression * lhs, Expression * rhs) : OpExpression('*', lhs, rhs) {}
+};
+
+class DivExpression : public OpExpression {
+ public:
+  DivExpression(Expression * lhs, Expression * rhs) : OpExpression('/', lhs, rhs) {}
+};
+
+#endif
